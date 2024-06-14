7ed928181c52ebe379c002d45393204613e6aa80
kirichenkods <kirichenkods@ya.ru>
2024-06-14 23:14:43 +0300

    fix lesson 12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -3,7 +3,6 @@ delete from STUDENT where ID in (select distinct STUDENT_ID from SUBJECT join MA
 --2
 delete from STUDENT where ID in (select distinct s.id from MARK m join student s on s.id = m.STUDENT_ID where m.MARK < 4);
 --3
-delete from PAYMENT where TYPE_ID in (select PAYMENTTYPE.ID from PAYMENTTYPE where NAME = 'DAILY');
 delete from PAYMENTTYPE where NAME = 'DAILY';
 --4
 delete from MARK where MARK < 7;
