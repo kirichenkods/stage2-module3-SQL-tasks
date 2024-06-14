5d51a250b8e0e43228e551bf25aec3f80b4e1bc2
kirichenkods <kirichenkods@ya.ru>
2024-06-14 23:01:23 +0300

    fix lesson 12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,8 +1,9 @@
 --1
-delete from STUDENT where ID IN (select distinct m.STUDENT_ID from SUBJECT sub join MARK m on sub.ID = m.SUBJECT_ID where sub.GRADE >= 4);
+delete from STUDENT where ID in (select distinct STUDENT_ID from SUBJECT join MARK M on SUBJECT.ID = M.SUBJECT_ID where GRADE >= 4);
 --2
-delete from STUDENT where ID IN (select distinct STUDENT_ID from MARK where MARK < 4);
+delete from STUDENT where ID in (select distinct s.id from MARK m join student s on s.id = m.STUDENT_ID where m.MARK < 4);
 --3
-delete from PAYMENTTYPE where NAME = 'Daily';
+delete from PAYMENT where TYPE_ID in (select PAYMENTTYPE.ID from PAYMENTTYPE where NAME = 'DAILY');
+delete from PAYMENTTYPE where NAME = 'DAILY';
 --4
 delete from MARK where MARK < 7;
--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,8 @@
-select * from student;
+alter table mark drop constraint mark_student_id_fkey;
+alter table mark add constraint mark_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
+alter table mark drop constraint mark_subject_id_fkey;
+alter table mark add constraint mark_subject_id_fkey foreign key (subject_id) references subject(id) on delete cascade;
+alter table payment drop constraint payment_type_id_fkey;
+alter table payment add constraint payment_type_id_fkey foreign key (type_id) references paymenttype(id) on delete cascade;
+alter table payment drop constraint payment_student_id_fkey;
+alter table payment add constraint payment_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
